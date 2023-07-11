2f27e907290df119795babf9b4e69db5f6840474
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-11 14:37:30 -0300

    Added new Project Esfera

---
+++ Exercicio URI 1011/Esfera/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double VOLUME, pi, r;
+            pi = 3.14159;
+
+            r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            VOLUME = (4.0 / 3) * pi * Math.Pow(r, 3);
+            Console.WriteLine("VOLUME = " + VOLUME.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
