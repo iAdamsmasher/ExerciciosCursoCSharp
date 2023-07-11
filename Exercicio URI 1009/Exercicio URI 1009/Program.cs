cea34814fc72b931a5651ad34f7ce7735a666cea
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-11 14:20:33 -0300

    New Folder URI1009

---
+++ Exercicio URI 1009/Exercicio URI 1009/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio_URI_1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salarioFixo, totalVenda, salarioTotal;
+
+            nome = Console.ReadLine();
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalVenda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //salarioTotal = totalVenda * 15.0 / 100 + salarioFixo;
+            salarioTotal = (totalVenda * 0.15) + salarioFixo;
+
+            Console.WriteLine("TOTAL = R$ " + salarioTotal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
