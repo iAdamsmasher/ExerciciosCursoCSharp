e5c55987e7290189e3e333d6e73031f4db9a2223
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-11 12:02:33 -0300

    First Commit

---
+++ Exercicio URI 1009/ExercicioUri1009/ExercicioUri1009/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioUri1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeDoVendedor;
+            double salariofixo, montanteTotal, totalReceber;
+
+            nomeDoVendedor = Console.ReadLine();
+            salariofixo = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            montanteTotal = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            //totalReceber = (((salariofixo * 15) / 100) + montanteTotal);
+            totalReceber = ((montanteTotal * 0.15) + salariofixo);
+
+            Console.WriteLine("TOTAL = R$ " + totalReceber.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
