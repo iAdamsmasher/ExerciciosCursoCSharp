0760edeedbb0cda22b663d52ca821aab7e055655
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-12 09:41:45 -0300

    New Exercice URI1013

---
+++ Exercicio URI 1013/Exercicio URI 1013/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio_URI_1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double consumoMédio, X, Y;
+            X = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            consumoMédio = (X / Y);
+
+            Console.WriteLine(consumoMédio.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
