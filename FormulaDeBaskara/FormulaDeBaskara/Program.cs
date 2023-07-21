dcbc96a44f5cdb88f534af3ec13c66f18135d1a2
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-21 10:58:58 -0300

    Finished Baskara formula

--- FormulaDeBaskara/FormulaDeBaskara/Program.cs
+++ FormulaDeBaskara/FormulaDeBaskara/Program.cs
@@ -14,6 +14,7 @@ namespace FormulaDeBaskara
             double A, B, C, delta, x1, x2;
 
             string[] vet = Console.ReadLine().Split(' ');
+           
             A = double.Parse(vet[0], CultureInfo.InvariantCulture);
             B = double.Parse(vet[1], CultureInfo.InvariantCulture);
             C = double.Parse(vet[2], CultureInfo.InvariantCulture);
@@ -21,12 +22,8 @@ namespace FormulaDeBaskara
             delta = (Math.Pow(B, 2.0) - (4 * A * C));
             x1 = ((-B + Math.Sqrt(delta)) / (2 * A));
             x2 = ((-B - Math.Sqrt(delta)) / (2 * A));
-            Console.WriteLine("A = " + A, CultureInfo.InvariantCulture);
-            Console.WriteLine("B = " + B, CultureInfo.InvariantCulture);
-            Console.WriteLine("C = " + C, CultureInfo.InvariantCulture);
-            Console.WriteLine("DELTA = " + delta, CultureInfo.InvariantCulture);
 
-            if (delta <= 0)
+            if (A <= 0 || B <=0 || C <=0 || delta <=0)
             {
                 Console.WriteLine("Impossivel calcular");
             }
@@ -34,6 +31,10 @@ namespace FormulaDeBaskara
             {
                 Console.WriteLine("R1 = " + x1.ToString("F5", CultureInfo.InvariantCulture));
                 Console.WriteLine("R2 = " + x2.ToString("F5", CultureInfo.InvariantCulture));
+               /* Console.WriteLine("DELTA = " + delta, CultureInfo.InvariantCulture);
+                Console.WriteLine("A = " + A, CultureInfo.InvariantCulture);
+                Console.WriteLine("B = " + B, CultureInfo.InvariantCulture);
+                Console.WriteLine("C = " + C, CultureInfo.InvariantCulture);*/
             }
         }
     }
