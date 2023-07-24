3ad2eff64f81c5d1f829823cb95d813da4f8ef53
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-24 10:00:15 -0300

    Switch Case added

---
+++ Program/Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua Idade:");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ta cada dia mais largo ai atras em man!!!");
+            Console.WriteLine("Mim de papai");
+            Console.WriteLine("Vou sentir saudade Brunin, voce mora no coração <3");
+            Console.ReadLine();
+        }
+    }
+}
