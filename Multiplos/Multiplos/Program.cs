adb12d5f93714ded2f004638d3f47215c0c4e561
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-25 12:30:56 -0300

    Tempo de Jogo

---
+++ Multiplos/Multiplos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+
+            if (B % A == 0 || A % B == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
---
+++ TempodeJogo/TempodeJogo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempodeJogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, result;
+            //1h = 60min = 3600s
+            string[] vet = Console.ReadLine().Split(' ');
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+            result = B - A;
+            if (A == B)
+            {
+                result = 24;
+
+            }
+            else if (B < A)
+            {
+                B += 24;
+                result = B - A;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + result + " HORA(S)");
+
+
+        }
+    }
+}
