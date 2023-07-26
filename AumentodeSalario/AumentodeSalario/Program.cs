3634011b23ae75e28f6e7996ebd2a330e9f2deb1
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-26 09:48:57 -0300

    Media_3

---
+++ AumentodeSalario/AumentodeSalario/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace AumentodeSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, novoSalario, sobrado;
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario <= 400.00)
+            {
+                sobrado = salario * 0.15;
+                novoSalario = sobrado + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + sobrado.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: 15 %");
+            }
+            else if (salario > 400.00 && salario <= 800.00)
+            {
+                sobrado = salario * 0.12;
+                novoSalario = sobrado + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + sobrado.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: 12 %");
+            }
+            else if (salario > 800.00 && salario <= 1200.00)
+            {
+                sobrado = salario * 0.10;
+                novoSalario = sobrado + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + sobrado.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: 10 %");
+            }
+            else if (salario > 1200.00 && salario <= 2000.00)
+            {
+                sobrado = salario * 0.07;
+                novoSalario = sobrado + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + sobrado.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: 7 %");
+            }
+            else if (salario > 2000.00)
+            {
+                sobrado = salario * 0.04;
+                novoSalario = sobrado + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + sobrado.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: 4 %");
+            }
+        }
+    }
+}
---
+++ Media_3/Media_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Media_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float N1, N2, N3, N4, media, notaExame, mediaExame; 
+            string[] vet = Console.ReadLine().Split(' ');
+            N1 = float.Parse(vet[0],CultureInfo.InvariantCulture);
+            N2 = float.Parse(vet[1],CultureInfo.InvariantCulture);
+            N3 = float.Parse(vet[2],CultureInfo.InvariantCulture);
+            N4 = float.Parse(vet[3],CultureInfo.InvariantCulture);
+
+            media = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / (2 + 3 + 4 + 1);
+
+            Console.WriteLine("Media = " + media.ToString("F1", CultureInfo.InvariantCulture));
+
+            if (media >= 7.0)
+            {
+                Console.WriteLine("Aluno aprovado.",CultureInfo.InvariantCulture);
+            }
+            else if (media < 5)
+            {
+                Console.WriteLine("Aluno reprovado.", CultureInfo.InvariantCulture);
+            }
+            else if (media >= 5 && media <= 6.9)
+            {
+                Console.WriteLine("Aluno em exame.");
+                notaExame = float.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                mediaExame = (notaExame + media) / 2;
+                if (mediaExame >= 5)
+                {
+                    Console.WriteLine("Nota do exame: " + notaExame.ToString("F1", CultureInfo.InvariantCulture));
+                    Console.WriteLine("Aluno aprovado.");
+                    Console.WriteLine("Media final: " + mediaExame.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado");
+                    Console.WriteLine("Media final: " + mediaExame.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+
+        }
+    }
+}
