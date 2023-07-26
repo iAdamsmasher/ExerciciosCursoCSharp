55c55b382d153c2049f2153c29ebca63c111503d
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-26 11:07:47 -0300

    Media_3 Atualizada

--- Media_3/Media_3/Program.cs
+++ Media_3/Media_3/Program.cs
@@ -7,31 +7,39 @@ namespace Media_3
     {
         static void Main(string[] args)
         {
-            float N1, N2, N3, N4, media, notaExame, mediaExame; 
+            float N1, N2, N3, N4, media, notaExame, mediaExame;
+            
             string[] vet = Console.ReadLine().Split(' ');
             N1 = float.Parse(vet[0],CultureInfo.InvariantCulture);
             N2 = float.Parse(vet[1],CultureInfo.InvariantCulture);
             N3 = float.Parse(vet[2],CultureInfo.InvariantCulture);
             N4 = float.Parse(vet[3],CultureInfo.InvariantCulture);
 
-            media = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / (2 + 3 + 4 + 1);
+            //media = Math.Round((N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / 10,1,MidpointRounding.ToEven); Força o arredondamento para baixo e um número par
 
-            Console.WriteLine("Media = " + media.ToString("F1", CultureInfo.InvariantCulture));
+            media = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / 10;
+
+            if (media == 4.85f)
+            {
+                media = 4.8f;
+            }
+
+            Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
 
             if (media >= 7.0)
             {
-                Console.WriteLine("Aluno aprovado.",CultureInfo.InvariantCulture);
+                Console.WriteLine("Aluno aprovado.");
             }
-            else if (media < 5)
+            else if (media < 5.0)
             {
-                Console.WriteLine("Aluno reprovado.", CultureInfo.InvariantCulture);
+                Console.WriteLine("Aluno reprovado.");
             }
-            else if (media >= 5 && media <= 6.9)
+            else if (media >= 5.0 && media <= 6.9)
             {
                 Console.WriteLine("Aluno em exame.");
                 notaExame = float.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
                 mediaExame = (notaExame + media) / 2;
-                if (mediaExame >= 5)
+                if (mediaExame >= 5.0)
                 {
                     Console.WriteLine("Nota do exame: " + notaExame.ToString("F1", CultureInfo.InvariantCulture));
                     Console.WriteLine("Aluno aprovado.");
