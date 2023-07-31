022bfaaad1ac166f86b8bf1bf747de0b3683cc68
iAdamSmasher <mafbpaiva@gmail.com>
2023-07-31 11:50:52 -0300

    New Exercise with While

---
+++ CrescenteEDecrescente/CrescenteEDecrescente/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrescenteEDecrescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string[] ordem = Console.ReadLine().Split(' ');
+            x = int.Parse(ordem[0]);
+            y = int.Parse(ordem[1]);
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                ordem = Console.ReadLine().Split(' ');
+                x = int.Parse(ordem[0]);
+                y = int.Parse(ordem[1]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
