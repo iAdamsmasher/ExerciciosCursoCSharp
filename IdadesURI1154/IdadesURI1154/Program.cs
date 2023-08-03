4f635cb9892543a7231456de1c8e8b4365b68adf
iAdamSmasher <mafbpaiva@gmail.com>
2023-08-03 14:06:45 -0300

    Added Soma de Pares Consecutivos

---
+++ IdadesURI1154/IdadesURI1154/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace IdadesURI1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double entrada = 0.0;
+            double soma = 0.0;
+            double cont = 0.0;
+            double media;
+            entrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (entrada >= 0)
+            {
+                soma = soma + entrada;
+                cont = cont + 1;
+                entrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            media = soma / cont;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ SomaDeParesConsecutivosURI1159/SomaDeParesConsecutivosURI1159/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SomaDeParesConsecutivosURI1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int saida = 0;
+            while (x != 0)
+            {
+                if (x % 2 == 0)
+                {
+                    saida = (x + (x + 2)) + (x + 4) + (x + 6) + (x + 8);
+
+                }
+                else if (x % 2 != 0)
+                {
+                    saida = (x + 1) + (x + 3) + (x + 5) + (x + 7) + (x + 9);
+                }
+                Console.WriteLine(saida);
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
