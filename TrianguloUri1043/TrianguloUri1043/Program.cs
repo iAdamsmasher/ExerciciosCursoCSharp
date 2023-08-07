fd2818391b03364e684ddde5c4ee44f5a11976e0
iAdamSmasher <mafbpaiva@gmail.com>
2023-08-07 10:16:23 -0300

    Added Triangulo

---
+++ TrianguloUri1043/TrianguloUri1043/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace TrianguloUri1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            double A = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            if (A < B + C && B < A + C && C < A + B )
+            {
+                double perimetro = A + B + C;
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+
+            }
+            else 
+            {
+                double area = ((A + B) * C) / 2;
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
