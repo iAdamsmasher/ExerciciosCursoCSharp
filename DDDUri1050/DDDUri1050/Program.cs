59b89b0ba014016325fc435bfb1e28c5bb89f7cd
iAdamSmasher <mafbpaiva@gmail.com>
2023-08-22 11:36:13 -0300

    Added new Exercise tabuada

---
+++ DDDUri1050/DDDUri1050/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DDDUri1050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int discagem = int.Parse(Console.ReadLine());
+ 
+            if (discagem == 61)
+            {
+                Console.WriteLine("Brasilia");
+            }
+            else if (discagem == 71)
+            {
+                Console.WriteLine("Salvador");
+            }
+            else if (discagem == 11)
+            {
+                Console.WriteLine("São Paulo");
+            }
+            else if (discagem == 21)
+            {
+                Console.WriteLine("Rio de Janeiro");
+            }
+            else if (discagem == 32)
+            {
+                Console.WriteLine("Juiz de Fora");
+            }
+            else if (discagem == 19)
+            {
+                Console.WriteLine("Campinas");
+            }
+            else if (discagem == 27)
+            {
+                Console.WriteLine("Vitoria");
+            }
+            else if (discagem == 31)
+            {
+                Console.WriteLine("Belo Horizonte");
+            }
+            else
+            {
+                Console.WriteLine("DDD nao cadastrado");
+            }
+        }
+    }
+}
---
+++ TabuadaURI1078/TabuadaURI1078/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TabuadaURI1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, i;
+            int tabuada;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= 10; i++)
+            {
+                tabuada = i * N;
+                Console.WriteLine(i + " x " +  N + " = " + tabuada);
+            }
+        }
+    }
+}
