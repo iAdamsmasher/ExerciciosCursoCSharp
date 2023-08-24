1fbcf4eda0a951111044f74aeb6d650f2013a8f0
iAdamSmasher <mafbpaiva@gmail.com>
2023-08-24 10:05:31 -0300

    New Exercise MêSUri1052

---
+++ MêsURI1052/MêsURI1052/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MêsURI1052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string mes = "";
+            switch (N)
+            {
+                case 1:
+                    mes = "January";
+                    break;
+                case 2:
+                    mes = "February";
+                    break;
+                case 3:
+                    mes = "March";
+                    break;
+                case 4:
+                    mes = "April";
+                    break;
+                case 5:
+                    mes = "May";
+                    break;
+                case 6:
+                    mes = "June";
+                    break;
+                case 7:
+                    mes = "July";
+                    break;
+                case 8:
+                    mes = "August";
+                    break;
+                case 9:
+                    mes = "September";
+                    break;
+                case 10:
+                    mes = "October";
+                    break;
+                case 11:
+                    mes = "November";
+                    break;
+                case 12:
+                    mes = "December";
+                    break;
+            }
+            Console.WriteLine(mes);
+        }
+    }
+}
---
+++ SomaDeImparesConsecutivosURI1071/SomaDeImparesConsecutivosURI1071/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SomaDeImparesConsecutivosURI1071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y, min, max, soma;
+            X = int.Parse(Console.ReadLine());
+            Y = int.Parse(Console.ReadLine());
+
+            if (X < Y)
+            {
+                min = X;
+                max = Y;
+            }
+            else
+            {
+                min = Y;
+                max = X;
+            }
+            soma = 0;
+             for (int i = min + 1; i < max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = soma + 1;
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
