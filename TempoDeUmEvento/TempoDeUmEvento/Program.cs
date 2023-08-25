5c318f42e239e614c09ae1594a7ed2743190b0d0
iAdamSmasher <mafbpaiva@gmail.com>
2023-08-25 10:15:17 -0300

    Tempo de um Evento AddedURI1061

---
+++ TempoDeUmEvento/TempoDeUmEvento/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempoDeUmEvento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas, minutos, segundos, N, resto, tempo, NN;
+            N = int.Parse(Console.ReadLine());
+
+            string[] evento = Console.ReadLine().Split(' ');
+            N = int.Parse(evento[1]);
+
+            evento = Console.ReadLine().Split(' ');
+            int hh = int.Parse(evento[0] + " : ");
+            int mm = int.Parse(evento[1] + " : ");
+            int ss = int.Parse(evento[2] + " : ");
+
+            evento = Console.ReadLine().Split(' ');
+            NN = int.Parse(evento[1]);
+
+            evento = Console.ReadLine().Split(' ');
+            int hh2 = int.Parse(evento[0] + " : ");
+            int mm2 = int.Parse(evento[1] + " : ");
+            int ss2 = int.Parse(evento[2] + " : ");
+
+        }
+    }
+}
