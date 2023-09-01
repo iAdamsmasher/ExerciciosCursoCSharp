cc0dce56c9c3a40fdfc1fc57d6a881affbd51cfe
iAdamSmasher <mafbpaiva@gmail.com>
2023-09-01 14:17:01 -0300

    New Exercise TempoDeUmEvento added

--- TempoDeUmEvento/TempoDeUmEvento/Program.cs
+++ TempoDeUmEvento/TempoDeUmEvento/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TempoDeUmEvento
 {
@@ -10,24 +6,39 @@ namespace TempoDeUmEvento
     {
         static void Main(string[] args)
         {
-            int horas, minutos, segundos, N, resto, tempo, NN;
-            N = int.Parse(Console.ReadLine());
+            int N, N2, inicio, fim;
+            string[] dd = Console.ReadLine().Split(' ');
+            N = int.Parse(dd[1]);
+    
+            dd = Console.ReadLine().Split(' ');
+            int hh = int.Parse(dd[0]);
+            int mm = int.Parse(dd[2]);
+            int ss = int.Parse(dd[4]);
 
-            string[] evento = Console.ReadLine().Split(' ');
-            N = int.Parse(evento[1]);
+            string[] dd2 = Console.ReadLine().Split(' ');
+            N2 = int.Parse(dd2[1]);
 
-            evento = Console.ReadLine().Split(' ');
-            int hh = int.Parse(evento[0] + " : ");
-            int mm = int.Parse(evento[1] + " : ");
-            int ss = int.Parse(evento[2] + " : ");
+            dd2 = Console.ReadLine().Split(' ');
+            int hh2 = int.Parse(dd2[0]);
+            int mm2 = int.Parse(dd2[2]);
+            int ss2 = int.Parse(dd2[4]);
 
-            evento = Console.ReadLine().Split(' ');
-            NN = int.Parse(evento[1]);
+            inicio = N * 86400 + hh * 3600 + mm * 60 + ss;
+            fim = N2 * 86400 + hh2 * 3600 + mm2 * 60 + ss2;
 
-            evento = Console.ReadLine().Split(' ');
-            int hh2 = int.Parse(evento[0] + " : ");
-            int mm2 = int.Parse(evento[1] + " : ");
-            int ss2 = int.Parse(evento[2] + " : ");
+            int duracao = fim - inicio;
+
+            int w = duracao / 86400;
+            int resto = duracao % 86400;
+            int x = resto / 3600;
+            resto = resto % 3600;
+            int Y = resto / 60;
+            int Z = resto % 60;
+
+            Console.WriteLine(w + " dia(s)");
+            Console.WriteLine(x + " hora(s)");
+            Console.WriteLine(Y + " minuto(s)");
+            Console.WriteLine(Z + " segundo(s)");
 
         }
     }
