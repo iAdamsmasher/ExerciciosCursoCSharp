4e55a207a281aae6c0d79ac04788614789744d63
iAdamSmasher <mafbpaiva@gmail.com>
2023-10-03 14:55:21 -0300

    New Exercises

---
+++ MaiorEPosicao_Beecrowd1080/MaiorEPosicao_Beecrowd1080/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MaiorEPosicao_Beecrowd1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, maior, posicao;
+            posicao = 1;
+            maior = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= 100; i++)
+            {
+                N = int.Parse(Console.ReadLine());
+                if (N > maior)
+                {
+                    maior = N;
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+        }
+    }
+}
